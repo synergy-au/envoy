cb65257324cf193b156db97a6afa0dab7d372875
Josh Vote <jjvote@gmail.com>
2023-02-07 15:13:35 +1100

    Early skeleton

---
+++ server/models/db/dnsp.py
@@ -0,0 +1,13 @@
+from base import Base
+from sqlalchemy import String
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+
+
+class DNSP(Base):
+    """Represents a Distribution Network Service Provider which is a top level entity that can access the majority
+    of entities "underneath" it"""
+
+    __tablename__ = "dnsp"
+    dnsp_id: Mapped[int] = mapped_column(primary_key=True)
+    email: Mapped[str] = mapped_column(String())
+    name: Mapped[str]
