a9248f1e9b08874f4e2bed700df07b423be24d2c
Josh Vote <jjvote@gmail.com>
2023-05-10 17:11:05 +1000

    Fixed doe models not being explicility imported

--- src/envoy/server/model/__init__.py
+++ src/envoy/server/model/__init__.py
@@ -1,6 +1,7 @@
 """The model refers to the internal DB model that is not exposed externally. Its purpose is to be compatibile with
 the various implemented schemas """
-from .base import *  # noqa
-from .aggregator import *  # noqa
-from .site import *  # noqa
-from .tariff import *  # noqa
+from .base import *  # noqa  # isort:skip
+from .aggregator import *  # noqa  # isort:skip
+from .site import *  # noqa  # isort:skip
+from .tariff import *  # noqa  # isort:skip
+from .doe import *  # noqa  # isort:skip
