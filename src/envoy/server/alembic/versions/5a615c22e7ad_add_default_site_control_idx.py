8729872b080ac42c26c2f27e62c3c7f00f19e523
Joe Crowley <joseph.crowley@synergy.net.au>
2025-06-23 11:23:24 +0800

    pointing latest migration at merge point

--- src/envoy/server/alembic/versions/5a615c22e7ad_add_default_site_control_idx.py
+++ src/envoy/server/alembic/versions/5a615c22e7ad_add_default_site_control_idx.py
@@ -1,7 +1,7 @@
 """add_default_site_control_idx
 
 Revision ID: 5a615c22e7ad
-Revises: 039bcb7dab76
+Revises: bd5eb8d95bee
 Create Date: 2025-06-19 16:18:13.875896
 
 """
@@ -10,7 +10,7 @@ from alembic import op
 
 # revision identifiers, used by Alembic.
 revision = "5a615c22e7ad"
-down_revision = "039bcb7dab76"
+down_revision = "bd5eb8d95bee"
 branch_labels = None
 depends_on = None
 
