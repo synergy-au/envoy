350fcd561a19510959855845d895fdb1c3186c11
Josh Vote <jjvote@gmail.com>
2026-07-04 11:52:39 +1000

    Reworked demo reset.sh to clear DB

--- demo/reset.sh
+++ demo/reset.sh
@@ -7,8 +7,10 @@ docker build --pull --no-cache -t envoy:latest -f ../Dockerfile.server ../
 
 HOST_UID=$(id -u) HOST_GID=$(id -g) docker compose down -v
 
+docker volume rm demo_postgres_data || true
+
 HOST_UID=$(id -u) HOST_GID=$(id -g) docker compose up -d --build
 
 echo ""
-echo "Stack is up. Smoke test with:"
-echo "  curl --cacert ./tls-termination/test_certs/testca.crt --cert ./tls-termination/test_certs/testdevice1.p12: --cert-type p12 https://localhost:8443/dcap"
+echo "Demo has been reset, run the following to start:"
+echo 'HOST_UID=$(id -u) HOST_GID=$(id -g) docker compose up --build'
