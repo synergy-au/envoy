8fc9275513eb3a3f0bbb716a07947de8d9ac610d
Joshua Vote <jjvote@gmail.com>
2026-07-30 11:02:17 +1000

    Fixed site archiving on delete being slow (#377)
    * Fixed site archiving on delete being slow
    
    * Fixed index

--- src/envoy/server/model/subscription.py
+++ src/envoy/server/model/subscription.py
@@ -44,7 +44,7 @@ class Subscription(Base):
         INTEGER, nullable=True
     )  # Represents the ID of a single resource being subscribed or if NULL, the list of all available resources
     scoped_site_id: Mapped[int | None] = mapped_column(
-        ForeignKey("site.site_id"), nullable=True
+        ForeignKey("site.site_id"), nullable=True, index=True
     )  # If set - this subscription is scoped to this specific site_id
 
     notification_uri: Mapped[str] = mapped_column(VARCHAR(length=2048))  # remote URI where notifications will be sent
@@ -73,7 +73,9 @@ class SubscriptionCondition(Base):
     __tablename__ = "subscription_condition"
 
     subscription_condition_id: Mapped[int] = mapped_column(primary_key=True)
-    subscription_id: Mapped[int] = mapped_column(ForeignKey("subscription.subscription_id", ondelete="CASCADE"))
+    subscription_id: Mapped[int] = mapped_column(
+        ForeignKey("subscription.subscription_id", ondelete="CASCADE"), index=True
+    )
 
     attribute: Mapped[ConditionAttributeIdentifier] = mapped_column(INTEGER)
     lower_threshold: Mapped[int] = mapped_column(
